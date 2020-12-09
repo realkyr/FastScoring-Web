0484010053bdecc756e5383f282222d55e01b4ba
unknown <60070075@kmitl.ac.th>
2020-12-09 22:55:16 +0700

    ♻️ refactor add form code

--- src/components/form/AddFormModal.js
+++ src/components/form/AddFormModal.js
@@ -51,6 +51,7 @@ export default class AddFormModal extends React.Component {
     this.formRef = db.collection('forms').doc()
     this._selectPDF = this._selectPDF.bind(this)
     this._stepContent = this._stepContent.bind(this)
+    this._send = this._send.bind(this)
   }
 
   async _selectPDF ({ file }) {
@@ -137,14 +138,35 @@ export default class AddFormModal extends React.Component {
                   placeholder="20"
                 />
               </Col>
+              <Col
+                xs={24}
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'flex-start'
+                }}
+              >
+                <Text>จำนวนตัวเลือกต่อข้อ: </Text>
+                <Input
+                  style={{ width: '100%' }}
+                  disabled
+                  value={5}
+                  placeholder="20"
+                />
+              </Col>
             </Row>
-            <canvas
-              style={{
-                width: '50%',
-                height: 'calc(50% / 2.031)',
+            <Row align="center">
+              <Col style={{
                 display: !this.state.isPDFRendered ? 'none' : 'block'
-              }}
-              id="pdf"></canvas>
+              }} xs={24} md={12}>
+                  <canvas
+                    style={{
+                      width: '20%'
+                    }}
+                    id="pdf"
+                  />
+              </Col>
+            </Row>
           </>
         )
       case 1: {
@@ -198,6 +220,99 @@ export default class AddFormModal extends React.Component {
     }
   }
 
+  async _send () {
+    const { cropper, step } = this.state
+
+    const storage = firebase.storage()
+    const user = firebase.auth().currentUser
+
+    const sheet = {}
+    if (step === 1) {
+      sheet.type = 'answersheet'
+      sheet.status = 'answer_status'
+      sheet.message = 'ansMessage'
+      sheet.pic = 'answerSheetJPG'
+      sheet.error = 'error_ans_msg'
+      sheet.result = 'answerResult'
+      sheet.analysed = 'analysed_answersheet_path'
+      sheet.path = 'answer_sheet_path'
+      sheet.payload = {
+        column: this.state.column,
+        amount: this.state.amount
+      }
+    } else {
+      sheet.type = 'student'
+      sheet.message = 'stuMessage'
+      sheet.status = 'stu_status'
+      sheet.path = 'student_path'
+      sheet.error = 'error_stu_msg'
+      sheet.result = 'studentResult'
+      sheet.analysed = 'analysed_stu_path'
+      sheet.payload = {
+        stu_column: 8
+      }
+    }
+
+    // add payload which contain the same data
+    sheet.payload = {
+      ...sheet.payload,
+      [sheet.status]: this.state[sheet.message] ? 'resend' : 'pending',
+      [sheet.path]: `/forms/${user.uid}/${this.formRef.id}_${sheet.type}.png`
+    }
+
+    const storageRef = storage.ref(
+      `/forms/${user.uid}/${this.formRef.id}_${sheet.type}.png`
+    )
+    this.unsub && this.unsub()
+    this.setState({
+      [sheet.pic]: cropper.getCroppedCanvas().toDataURL()
+    })
+    await this.formRef.set(
+      sheet.payload,
+      { merge: true }
+    )
+    storageRef.putString(
+      cropper.getCroppedCanvas().toDataURL(),
+      'data_url'
+    )
+    this.unsub = this.formRef.onSnapshot(async s => {
+      const info = s.data()
+      switch (info[sheet.status]) {
+        case 'pending':
+        case 'resend':
+        case 'analysing':
+          this.setState({
+            stepStatus: 'process',
+            isLoading: true,
+            [sheet.message]: info[sheet.status]
+          })
+          break
+        case 'error':
+          this.setState({
+            stepStatus: 'error',
+            isLoading: false,
+            [sheet.message]: info[sheet.error]
+          })
+          break
+        case 'pass': {
+          const resultRef = storage.ref(
+            info[sheet.analysed]
+          )
+          const url = await resultRef.getDownloadURL()
+          this.setState({
+            [sheet.result]: url,
+            stepStatus: 'finish',
+            isLoading: false,
+            [sheet.message]: 'Compatible'
+          })
+          break
+        }
+        default:
+          break
+      }
+    })
+  }
+
   async _stepChange (step, resend) {
     const canvas = document.getElementById('pdf')
     if (canvas) {
@@ -205,138 +320,27 @@ export default class AddFormModal extends React.Component {
         pdfFileImg: canvas.toDataURL('image/jpeg')
       })
     }
-    const { cropper } = this.state
-
     const storage = firebase.storage()
-    const user = firebase.auth().currentUser
-    const storageRef = storage.ref(
-      `/forms/${user.uid}/${this.formRef.id}_answersheet.png`
-    )
-    const storageStuRef = storage.ref(
-      `/forms/${user.uid}/${this.formRef.id}_student.png`
-    )
 
     switch (step) {
+      case 0:
+        this.setState({
+          step
+        }, () => {
+          // if pdf file selected load one
+          if (this.state.pdfFile) this._selectPDF({ file: this.state.pdfFile })
+        })
+        break
       case 1:
-        this.setState({ step }, this._initCropper)
-        break
       case 2:
-        if (!resend && this.state.stepStatus === 'finish') {
-          this.setState(
-            {
-              step,
-              stepStatus: 'process'
-            },
-            this._initCropper
-          )
-          break
-        }
-        this.unsub && this.unsub()
-        this.setState({
-          answerSheetJPG: cropper.getCroppedCanvas().toDataURL()
-        })
-        await this.formRef.set(
-          {
-            answer_status: this.state.ansMessage ? 'resend' : 'pending',
-            column: this.state.column,
-            amount: this.state.amount,
-            answer_sheet_path: `/forms/${user.uid}/${this.formRef.id}_answersheet.png`
-          },
-          { merge: true }
-        )
-        storageRef.putString(cropper.getCroppedCanvas().toDataURL(), 'data_url')
-        this.unsub = this.formRef.onSnapshot(async s => {
-          const info = s.data()
-          switch (info.answer_status) {
-            case 'pending':
-            case 'resend':
-            case 'analysing':
-              this.setState({
-                stepStatus: 'process',
-                isLoading: true,
-                ansMessage: info.answer_status
-              })
-              break
-            case 'error':
-              this.setState({
-                stepStatus: 'error',
-                isLoading: false,
-                ansMessage: info.error_ans_msg
-              })
-              break
-            case 'pass': {
-              const answerResultRef = storage.ref(
-                info.analysed_answersheet_path
-              )
-              const url = await answerResultRef.getDownloadURL()
-              this.setState({
-                answerResult: url,
-                stepStatus: 'finish',
-                isLoading: false,
-                ansMessage: null
-              })
-              break
-            }
-            default:
-              break
-          }
-        })
-        break
       case 3:
-        if (!resend && this.state.stepStatus === 'finish') {
-          this.setState({ step, stepStatus: 'process' }, this._initCropper)
-          break
-        }
-        this.unsub && this.unsub()
-        this.setState({
-          answerSheetJPG: cropper.getCroppedCanvas().toDataURL()
-        })
-        await this.formRef.set(
+        this.setState(
           {
-            stu_column: 8,
-            stu_status: this.state.stuMessage ? 'resend' : 'pending',
-            student_path: `/forms/${user.uid}/${this.formRef.id}_student.png`
+            step,
+            stepStatus: 'process'
           },
-          { merge: true }
+          this._initCropper
         )
-        storageStuRef.putString(
-          cropper.getCroppedCanvas().toDataURL(),
-          'data_url'
-        )
-        this.unsub = this.formRef.onSnapshot(async s => {
-          const info = s.data()
-          switch (info.stu_status) {
-            case 'pending':
-            case 'resend':
-            case 'analysing':
-              this.setState({
-                stepStatus: 'process',
-                isLoading: true,
-                stuMessage: info.stu_status
-              })
-              break
-            case 'error':
-              this.setState({
-                stepStatus: 'error',
-                isLoading: false,
-                stuMessage: info.error_stu_msg
-              })
-              break
-            case 'pass': {
-              const ResultRef = storage.ref(info.analysed_stu_path)
-              const url = await ResultRef.getDownloadURL()
-              this.setState({
-                studentResult: url,
-                stepStatus: 'finish',
-                isLoading: false,
-                stuMessage: null
-              })
-              break
-            }
-            default:
-              break
-          }
-        })
         break
       case 4: {
         const { pdfFile, formName } = this.state
@@ -395,14 +399,30 @@ export default class AddFormModal extends React.Component {
 
     return (
       <Modal
+        style={{
+          top: '50px'
+        }}
         visible={visible}
         title="Add Form"
         // onOk={this.handleOk}
         onCancel={toggleModal}
         footer={[
-          <Button key="previous">ย้อนกลับ</Button>,
           <Button
-            disabled={this.state.isLoading}
+            key="previous"
+            disabled={
+              this.state.isLoading || this.state.step === 0
+            }
+            onClick={() => this._stepChange(this.state.step - 1)}
+          >ย้อนกลับ</Button>,
+          <Button
+            key="send"
+            disabled={
+              this.state.isLoading || [0, 3].includes(this.state.step)
+            }
+            onClick={this._send}
+          >ส่ง</Button>,
+          <Button
+            disabled={this.state.isLoading || ([1, 2].includes(this.state.step) && this.state.stepStatus !== 'finish')}
             onClick={() => this._stepChange(this.state.step + 1)}
             type="primary"
             key="next">
@@ -410,37 +430,45 @@ export default class AddFormModal extends React.Component {
           </Button>
         ]}
         width={1000}>
-        <Steps size="small" status={this.state.stepStatus} current={step}>
-          <Step title="เลือก PDF" />
-          <Step
-            title="Crop ส่วนที่ฝนคำตอบ"
-            description={this.state.ansMessage}
-            icon={
-              this.state.isLoading && this.state.step === 1
-                ? (
-                    <LoadingOutlined />
-                  )
-                : null
-            }
-          />
-          <Step
-            title="Crop ส่วนที่ฝนรหัสนักศึกษา"
-            description={this.state.stuMessage}
-            icon={
-              this.state.isLoading && this.state.step === 2
-                ? (
-                    <LoadingOutlined />
-                  )
-                : null
-            }
-          />
-          <Step title="ตั้งชื่อฟอร์ม" />
-        </Steps>
+        <div style={{
+          padding: '10px',
+          position: 'sticky',
+          zIndex: 1001,
+          background: 'white',
+          top: 0
+        }}>
+          <Steps size="small" status={this.state.stepStatus} current={step}>
+            <Step title="เลือก PDF" />
+            <Step
+              title="Crop ส่วนที่ฝนคำตอบ"
+              description={this.state.ansMessage}
+              icon={
+                this.state.isLoading && this.state.step === 1
+                  ? (
+                      <LoadingOutlined />
+                    )
+                  : null
+              }
+            />
+            <Step
+              title="Crop ส่วนที่ฝนรหัสนักศึกษา"
+              description={this.state.stuMessage}
+              icon={
+                this.state.isLoading && this.state.step === 2
+                  ? (
+                      <LoadingOutlined />
+                    )
+                  : null
+              }
+            />
+            <Step title="ตั้งชื่อฟอร์ม" />
+          </Steps>
+        </div>
         <div className="steps-content">
           {this._stepContent()}
           <div
             style={{
-              display: !this.state.pdfFileImg || this.state.step === 3 ? 'none' : 'block'
+              display: !this.state.pdfFileImg || [0, 3].includes(this.state.step) ? 'none' : 'block'
             }}>
             <Image
               id="image-cropper"
